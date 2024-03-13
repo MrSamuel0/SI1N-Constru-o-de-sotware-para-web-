9f8bac0ca6a5d9dbdbbf394fa3dd6689d5800551
Samuel Manhani Salles <151402780+MrSamuel0@users.noreply.github.com>
2024-03-12 22:38:35 -0300

    Add files via upload

---
+++ Aula 05/seila2.0.js
@@ -0,0 +1,19 @@
+function(){
+
+    var x = prompt('diga o valor de x: ')
+
+    console.log('eu não sei de nada')
+
+    if (x > 2){
+        console.log('x é maior que dois.')
+    }
+    else if (x <= 2){
+        console.log('x é menor ou igual a dois.')
+    }
+    else if (x >= 2){
+        console.log('x é menor ou igual a dois.')
+    }
+    else if (x != 2){
+        console.log('x é diferente de dois.')
+    }
+}
